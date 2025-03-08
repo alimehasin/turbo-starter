475ae476839330c22e1eb66e5c3ae37dbf3f8a99
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-03-08 16:59:53 +0300

    chore: pagination and sorting schemas shouldn't be optional by default

--- apps/dashboard/src/schemas/base.ts
+++ apps/dashboard/src/schemas/base.ts
@@ -1,15 +1,11 @@
 import { z } from 'zod';
 
-export const paginationSchema = z
-  .object({
-    page: z.number(),
-    pageSize: z.number(),
-  })
-  .optional();
+export const paginationSchema = z.object({
+  page: z.number(),
+  pageSize: z.number(),
+});
 
-export const sortingSchema = z
-  .object({
-    column: z.string(),
-    direction: z.enum(['asc', 'desc']),
-  })
-  .optional();
+export const sortingSchema = z.object({
+  column: z.string(),
+  direction: z.enum(['asc', 'desc']),
+});

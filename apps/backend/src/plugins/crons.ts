4ebd28863029f06c7c522e4dc65184a9ee77aa5d
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-03-13 01:22:12 +0300

    chore: remove unused dayjs import and related logic from storage-cleaner cron

--- apps/backend/src/plugins/crons.ts
+++ apps/backend/src/plugins/crons.ts
@@ -3,8 +3,6 @@ import { cron as elysiaCron } from '@elysiajs/cron';
 import type { Prisma } from '@prisma/client';
 import { prisma } from '@repo/db';
 import { deleteObjects } from '@repo/storage';
-import dayjs from 'dayjs';
-
 export const crons = elysiaCron({
   name: 'storage-cleaner',
   pattern: '0 4 * * *',
@@ -14,7 +12,6 @@ export const crons = elysiaCron({
     // Put Your filter here
     const where: Prisma.FileWhereInput = {
       userId: '0',
-      createdAt: { lt: dayjs().subtract(6, 'hour').toDate() },
     };
 
     const files = await prisma.file.findMany({ where });

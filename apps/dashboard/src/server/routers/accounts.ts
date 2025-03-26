46d3e702348db54eedc49175d0c31951987832b5
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-03-27 02:30:30 +0300

    fix: translation in accounts endpoints

--- apps/dashboard/src/server/routers/accounts.ts
+++ apps/dashboard/src/server/routers/accounts.ts
@@ -7,19 +7,31 @@ import { z } from 'zod';
 export const accounts = createTRPCRouter({
   login: publicProcedure
     .input(z.object({ username: z.string(), password: z.string() }))
-    .mutation(async ({ ctx: { prisma }, input: { username, password } }) => {
+    .mutation(async ({ ctx: { t, prisma }, input: { username, password } }) => {
       const admin = await prisma.admin.findUnique({
         where: { username },
       });
 
       if (!admin) {
-        throw new TRPCError({ code: 'NOT_FOUND', message: 'Admin not found' });
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: t({
+            en: 'Admin not found',
+            ar: 'المدير غير موجود',
+          }),
+        });
       }
 
       const isPasswordValid = await bcrypt.compare(password, admin.password);
 
       if (!isPasswordValid) {
-        throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Invalid password' });
+        throw new TRPCError({
+          code: 'UNAUTHORIZED',
+          message: t({
+            en: 'Invalid password',
+            ar: 'كلمة المرور غير صحيحة',
+          }),
+        });
       }
 
       const token = await signJwt({

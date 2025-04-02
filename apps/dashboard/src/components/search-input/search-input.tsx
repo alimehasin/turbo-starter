50255c5688ef522371def80832f6b576143331c7
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-04-03 00:55:49 +0300

    feat: add search-input component

---
+++ apps/dashboard/src/components/search-input/search-input.tsx
@@ -0,0 +1,28 @@
+import { InputClearButton, TextInput, type TextInputProps, rem } from '@mantine/core';
+import { IconSearch } from '@tabler/icons-react';
+import { useTranslations } from 'next-intl';
+
+interface SearchInputProps extends Omit<TextInputProps, 'onChange'> {
+  onChange: (value: string) => void;
+}
+
+export function SearchInput({
+  value,
+  onChange,
+  placeholder,
+  ...props
+}: SearchInputProps) {
+  const t = useTranslations();
+
+  return (
+    <TextInput
+      w={260}
+      leftSection={<IconSearch />}
+      rightSectionWidth={rem(40)}
+      placeholder={placeholder || t('search')}
+      rightSection={value && <InputClearButton onClick={() => onChange('')} />}
+      onChange={(event) => onChange(event.target.value)}
+      {...props}
+    />
+  );
+}

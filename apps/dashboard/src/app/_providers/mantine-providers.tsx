002245661e780edf755028d19e7021e6939b1617
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-05-28 02:14:24 +0300

    chore(dashboard): remove unused carousel styles

--- apps/dashboard/src/app/_providers/mantine-providers.tsx
+++ apps/dashboard/src/app/_providers/mantine-providers.tsx
@@ -4,7 +4,6 @@ import '@/styles/globals.css';
 import '@mantine/core/styles.layer.css';
 import '@mantine/dates/styles.layer.css';
 import '@mantine/charts/styles.layer.css';
-import '@mantine/carousel/styles.layer.css';
 import '@mantine/dropzone/styles.layer.css';
 import '@mantine/notifications/styles.layer.css';
 import 'mantine-datatable/styles.layer.css';

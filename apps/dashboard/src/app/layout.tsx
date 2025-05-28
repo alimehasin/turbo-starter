d8edcfe5d931575a36f9bbbb229a48311619d41f
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-05-28 15:34:58 +0300

    feat: add dayjs-provider

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ColorSchemeScript, mantineHtmlProps } from '@mantine/core';
 import type { Metadata } from 'next';
 import { getLocale } from 'next-intl/server';
+import { DayjsProvider } from './_providers/dayjs-provider';
 import { MantineProviders } from './_providers/mantine-providers';
 import { NextIntlProvider } from './_providers/nex-intl-provider';
 import { TrpcProvider } from './_providers/trpc-provider';
@@ -28,9 +29,11 @@ export default async function RootLayout({
       <body>
         <NextIntlProvider>
           <TrpcProvider>
-            <MantineProviders locale={locale} initialDirection={dir}>
-              {children}
-            </MantineProviders>
+            <DayjsProvider locale={locale}>
+              <MantineProviders locale={locale} initialDirection={dir}>
+                {children}
+              </MantineProviders>
+            </DayjsProvider>
           </TrpcProvider>
         </NextIntlProvider>
       </body>

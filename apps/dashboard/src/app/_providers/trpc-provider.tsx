99dcb847ae9b16cedd1e862044067b520a71f6eb
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-06-06 10:44:49 +0300

    feat(trpc): refactor TRPC provider to use a custom query client for enhanced error handling

--- apps/dashboard/src/app/_providers/trpc-provider.tsx
+++ apps/dashboard/src/app/_providers/trpc-provider.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import { api } from '@/server/callers/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createQueryClient } from '@/server/query-client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { useState } from 'react';
 import superjson from 'superjson';
 
 export function TrpcProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => createQueryClient());
   const [trpcClient] = useState(() => {
     return api.createClient({
       links: [

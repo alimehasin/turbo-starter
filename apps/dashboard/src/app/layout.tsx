9a894b4f391b928c6e337099b283c6a80381ffbb
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-09-18 09:40:33 +0300

    feat: update metadata generation to use translations for title and description in layout component

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,16 +1,20 @@
 import { ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
 import type { Metadata } from "next";
-import { getLocale } from "next-intl/server";
+import { getLocale, getTranslations } from "next-intl/server";
 import { DayjsProvider } from "@/providers/dayjs-provider";
 import { MantineProviders } from "@/providers/mantine-providers";
 import { NextIntlProvider } from "@/providers/nex-intl-provider";
 import { QueryClientProvider } from "@/providers/query-client-provider";
 
-export const metadata: Metadata = {
-  title: "Turborepo Starter",
-  description: "Turborepo Starter with Next.js, Mantine and more",
-  robots: "noindex, nofollow",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations();
+
+  return {
+    title: t("meta.title"),
+    description: t("meta.description"),
+    robots: "noindex, nofollow",
+  };
+}
 
 export default async function RootLayout({
   children,

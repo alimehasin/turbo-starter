3c13e652b940cd896c30af91982fb620dbcdf687
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-09-24 21:15:04 +0300

    refactor(backend): rename app dir to api

--- apps/backend/src/routes/admin.ts
+++ apps/backend/src/routes/admin.ts
@@ -1,6 +1,6 @@
 import { Elysia } from 'elysia';
-import { files } from '@/app/files/files.admin.route';
-import { governorates } from '@/app/governorates/governorates.admin.route';
+import { files } from '@/api/files/files.admin.route';
+import { governorates } from '@/api/governorates/governorates.admin.route';
 import { betterAuth } from '@/plugins/better-auth';
 
 export const adminRoutes = new Elysia({ prefix: '/admin', tags: ['Admin'] })
--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -1,6 +1,6 @@
 import { Elysia } from 'elysia';
-import { files } from '@/app/files/files.user.route';
-import { governorates } from '@/app/governorates/governorates.user.route';
+import { files } from '@/api/files/files.user.route';
+import { governorates } from '@/api/governorates/governorates.user.route';
 
 export const userRoutes = new Elysia({ prefix: '/user', tags: ['User'] })
 

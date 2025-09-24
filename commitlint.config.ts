9a782edcdc5fc136c59ebc918379ccf6dd34c846
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-09-24 13:15:40 +0300

    chore: increase header-max-length rule in commitlint configuration from 128 to 144

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,6 +4,6 @@ import { RuleConfigSeverity } from '@commitlint/types';
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [RuleConfigSeverity.Error, 'always', 128],
+    'header-max-length': [RuleConfigSeverity.Error, 'always', 144],
   },
 } as UserConfig;

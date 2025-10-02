7f12408a717d2169afd08934df8a2d58898fb445
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2025-10-02 14:53:10 +0300

    feat: enhance query client error handling with notifications for validation errors

--- apps/dashboard/src/providers/query-client-provider.tsx
+++ apps/dashboard/src/providers/query-client-provider.tsx
@@ -1,17 +1,53 @@
 'use client';
 
+import { List, ListItem } from '@mantine/core';
+import { showNotification } from '@mantine/notifications';
 import {
   QueryClient,
   QueryClientProvider as ReactQueryClientProvider,
 } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import { HTTPError } from 'ky';
+import { useTranslations } from 'next-intl';
+import { useState } from 'react';
+import { useNotifications } from '@/hooks/use-notifications';
 
 export function QueryClientProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const t = useTranslations();
+  const n = useNotifications();
+
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      defaultOptions: {
+        mutations: {
+          onError: async (err) => {
+            if (err instanceof HTTPError) {
+              const errorBody = await err.response.json();
+              if (errorBody.errorCode === 'ValidationError') {
+                showNotification({
+                  color: 'red',
+                  title: t('_.error'),
+                  message: (
+                    <List>
+                      {errorBody.errors.map((error: string) => (
+                        <ListItem key={error}>{error}</ListItem>
+                      ))}
+                    </List>
+                  ),
+                });
+              } else {
+                n.error(errorBody.message);
+              }
+            }
+          },
+        },
+      },
+    });
+  });
+
   return (
     <ReactQueryClientProvider client={queryClient}>
       {children}
